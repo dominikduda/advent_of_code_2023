548c5b30c5ad00c5d8669e2745e373e2d8e4f831
Dominik Duda <gospodorz@gmail.com>
2023-12-04 10:20:22 +0100

    Autoformat

--- solution_3a.rb
+++ solution_3a.rb
@@ -16,7 +16,6 @@ def adjacent_chars(matrix, row_index, number_beginning_index, number_end_index)
   chars << matrix[row_index][number_beginning_index - 1]
   chars << matrix[row_index + 1][number_beginning_index - 1] if row_index < matrix.length - 1
 
-
   (number_beginning_index..number_end_index).each do |digit_index|
     chars << matrix[row_index - 1][digit_index] if row_index > 0
     chars << matrix[row_index + 1][digit_index] if row_index < matrix.length - 1
@@ -29,44 +28,37 @@ def adjacent_chars(matrix, row_index, number_beginning_index, number_end_index)
   chars.compact
 end
 
-
 def calculate_sum(matrix)
   sum = 0
   matrix.each_with_index do |row, row_index|
     number_beginning = nil
     number_end = nil
     row.each_with_index do |char, char_index|
-      if number_beginning == nil && integer?(char)
-        number_beginning = char_index
-      end
-      if number_beginning && number_end == nil
-        if !integer?(char)
-          number_end = char_index - 1
-        end
-        if integer?(char) && char_index == row.length - 1
-          number_end = char_index
-        end
+      number_beginning = char_index if number_beginning.nil? && integer?(char)
+      if number_beginning && number_end.nil?
+        number_end = char_index - 1 unless integer?(char)
+        number_end = char_index if integer?(char) && char_index == row.length - 1
       end
 
+      next unless !number_beginning.nil? && !number_end.nil? && (!integer?(char) || char_index == row.length - 1)
 
-      if number_beginning != nil && number_end != nil && (!integer?(char) || char_index == row.length - 1)
-        if adjacent_chars(matrix, row_index, number_beginning, number_end).any? { |char| char != '.' && !integer?(char) }
-          number = ''
-          (number_beginning..number_end).each do |digit_index|
-            number << matrix[row_index][digit_index]
-          end
-          number = number.to_i
-          sum += number
+      if adjacent_chars(matrix, row_index, number_beginning, number_end).any? do |char|
+           char != '.' && !integer?(char)
+         end
+        number = ''
+        (number_beginning..number_end).each do |digit_index|
+          number << matrix[row_index][digit_index]
         end
-
-        number_beginning = nil
-        number_end = nil
+        number = number.to_i
+        sum += number
       end
+
+      number_beginning = nil
+      number_end = nil
     end
   end
   sum
 end
-
 
 require_relative 'tests_3a'
 
